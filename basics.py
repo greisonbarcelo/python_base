b8755687c8fb7e0ef9cc28946a65c624ac63c05a
greisonbarcelo <barcelogreison@gmail.com>
2026-02-08 16:42:45 +0800

    Functions basics in python...

--- basics.py
+++ basics.py
@@ -15,14 +15,30 @@
 # print(armored_health)
 # print(player_health)
 
-player_health = 100
-poison_damage = -10
+# player_health = 100
+# poison_damage = -10
 
-player_poison_health = player_health + poison_damage
+# player_poison_health = player_health + poison_damage
 
-print(player_poison_health)
+# print(player_poison_health)
 
 # this is a single line comment. It can be used to write a brief explanation or note about a specific line of code or a small section of code.
 """
 This is a multi-comment block. It can be used to write multiple lines of comments without needing to use the # symbol at the beginning of each line. This is useful for writing longer explanations or notes in your code.
 """
+
+# functions
+
+def area_of_circle(radius):
+    pie = 3.14
+    area = pie * radius * radius
+    return area
+
+sword_length = 1.0
+spear_length = 2.0
+
+sword_area = area_of_circle(sword_length)
+spear_area = area_of_circle(spear_length)
+
+print(f"Sword area is ${sword_area}")
+print(f"Spear area is ${spear_area}")
