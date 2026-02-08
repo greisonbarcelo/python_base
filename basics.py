a73c96f0a7f07dd479c47b11a3e17bb7cced1a4a
greisonbarcelo <barcelogreison@gmail.com>
2026-02-08 16:27:27 +0800

    More revisiting basics of python before moving on to advanced projects

--- basics.py
+++ basics.py
@@ -1,7 +1,27 @@
-a = 6
+# a = 6
 
-a = a + 2
+# a = a + 2
 
-print(a)
+# print(a)
 
-print("Hello, World!")
+# print("Hello, World!")
+
+
+# player_health = 1000
+# armor_multiplier = 2
+
+# armored_health = player_health * armor_multiplier
+
+# print(armored_health)
+# print(player_health)
+
+player_health = 100
+poison_damage = -10
+
+player_poison_health = player_health + poison_damage
+
+print(player_poison_health)
+
+"""
+This is a comment block. It can be used to write multiple lines of comments without needing to use the # symbol at the beginning of each line. This is useful for writing longer explanations or notes in your code.
+"""
